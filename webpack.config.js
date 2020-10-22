5de9343f372e2b0905dc6a554a0289be8dd933ba
Alex Semochkin <fennyflop@gmail.com>
2020-10-22 12:05:57 +0300

    babel installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
-const path = require('path'); // подключаем path к конфигу вебпак
+const path = require('path');
 
 module.exports = {
     entry: { main: './src/pages/index.js' },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
-    }
-}
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+    module: {
+    rules: [ 
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: '/node_modules/'
+      }
+      ]
+  }
+};
