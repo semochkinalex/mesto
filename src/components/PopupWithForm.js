ccd3b6bdd853d203b78354a7ad225db462844302
Alex Semochkin <fennyflop@gmail.com>
2020-10-26 11:01:58 +0300

    popup with form improved

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,25 +1,29 @@
 import Popup from './Popup.js';
-export default class PopupWithForm extends Popup{
-    constructor (popupSelector, callback){
+export default class PopupWithForm extends Popup {
+    constructor(popupSelector, callback) {
         super(popupSelector);
         this._popup = document.querySelector(popupSelector);
         this._callback = callback
-    }   
-    _getInputValues () {
-        const inputs = Object.assign(this._popup.querySelectorAll('.form__item'));
-        return inputs;
+    }
+    _getInputValues() {
+        this._inputList = this._popup.querySelectorAll('.form__item');
+        this._formValues = {};
+        this._inputList.forEach(input => {
+            this._formValues[input.name] = input.value;
+        });
+        return this._formValues;
     }
     setEventListeners() {
         this._popup.addEventListener('submit', () => {
             this._callback()
         });
         super.setEventListeners();
-    }  
-    close() { 
+    }
+    close() {
         super.close();
-        const inputs = this._getInputValues();
+        const inputs = document.querySelectorAll('.form__item');
         inputs.forEach((input) => {
             input.value = '';
         })
-    } 
+    }
 }
